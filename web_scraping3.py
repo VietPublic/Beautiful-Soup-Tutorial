02c7a95da22ea2b475172d27ce9020aa1ad62384
VietPublic <vietnampublic@gmail.com>
2024-11-24 23:20:27 +0700

    Update web_scraping3.py
    coinmarkecap now using span tag

--- web_scraping3.py
+++ web_scraping3.py
@@ -13,7 +13,7 @@ prices = {}
 for tr in trs[:10]:
 	name, price = tr.contents[2:4]
 	fixed_name = name.p.string
-	fixed_price = price.a.string
+	fixed_price = price.span.getText()
 
 	prices[fixed_name] = fixed_price
 
